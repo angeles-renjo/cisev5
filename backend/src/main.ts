9680233584ea8b793cc0d11586ef2d7c03d72409
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2023-10-05 23:07:13 +1300

    change syntax

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://cisev5.vercel.app/vercel.app',
+    origin: 'https://cisev5.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
