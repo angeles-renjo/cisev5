d682bbcc962ae6e2037323f253dbf0abda4778b8
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2023-10-05 23:00:40 +1300

    change url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://cise-frontend-h8i1dzse9-gxc1998github.vercel.app',
+    origin: 'https://cisev5-g8w8.vercel.app/vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
