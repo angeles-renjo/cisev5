851d3f27addfdb13cec092dc1d66cb19f22495d7
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2023-10-06 03:13:22 +1300

    vercel.json config and main.ts
    changes made

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
