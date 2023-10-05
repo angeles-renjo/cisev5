94baf813cd9c7836591674f89275d15cb44d7cc2
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2023-10-05 22:40:56 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://cisev5-g8w8.vercel.app/',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
+
   dotenv.config();
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
