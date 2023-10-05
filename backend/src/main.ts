46c8071d27463f9a0bc8131df3346c9ff11f7a96
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2023-10-06 00:00:26 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https:/cisev5-24gr1ixbj-my-team-6b67d362.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   dotenv.config();
   const PORT = process.env.PORT || 8000;
